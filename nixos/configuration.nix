c3ce10dd27e17bd6379e588b6523cae900e167b1
alice <aliceecity@proton.me>
2025-12-25 11:41:18 +0100

    8 current   2025-12-25 11:41:15  25.05.812880.4c8cdd5b1a63  6.12.58                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -105,6 +105,7 @@
   environment.systemPackages = with pkgs; [
    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    typst
+   gcc
    fastfetch
    wget
   ];
