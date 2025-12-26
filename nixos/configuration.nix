99cd7c08236ef08d282f10d46b67bcfd47ce62f8
alice <aliceecity@proton.me>
2025-12-26 11:19:10 +0100

    12 current 2025-12-26 11:19:07

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -93,7 +93,8 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"]
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   environment.systemPackages = with pkgs; [
     tmux
     neovim
