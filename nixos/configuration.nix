25b837aeada84c255d56dad2515ee2fdc5df28f1
alice <aliceecity@proton.me>
2025-12-26 21:26:09 +0100

    20 2025-12-26 16:55:26

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,7 +85,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sancho = {
     isNormalUser = true;
-    shell = "zsh";
+    shell = pkgs.zsh;
     description = "sancho";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
