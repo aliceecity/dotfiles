c9276cce7d5b82abcb10312fcaf154ac17d3871d
alice <aliceecity@proton.me>
2025-12-30 19:14:42 +0100

    Set up home manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -96,15 +96,9 @@
     shell = pkgs.zsh;
     description = "sancho";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      qview
-      streamlink
-      btop
-      vlc
-      discordo
-      chatterino7
-    ];
   };
+
+  programs.zsh.enable = true;
 
   programs.nh = {
     enable = true;
@@ -113,21 +107,6 @@
       extraArgs = "--keep-since 4d --keep 10";
     };
   };
-
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-        l = "ls -lh";
-        la = "ls -lha";
-        v = "nvim";
-        vi = "nvim";
-        ".." = "cd ..";
-      };
-    enableLsColors = true;
-    enableCompletion = true;
-  };
-
-  programs.firefox.enable = true;
 
   programs.steam = {
     enable = true;
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,13 +3,30 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {self, nixpkgs, ... }@inputs: {
+  outputs = {self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       lethe = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules = [ 
+          ./configuration.nix 
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+
+            home-manager.users.sancho = import ./home.nix;
+          }
+        ];
       };
     };
   };
