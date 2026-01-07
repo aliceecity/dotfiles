cd5bbb25498ec4f2abc5a31f9b93130368545dbd
alice <aliceecity@proton.me>
2026-01-07 15:22:26 +0100

    Added better functionality for cd-go-to.sh

--- scripts/cd-go-to.sh
+++ scripts/cd-go-to.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+tty=$(tmux display-message -p '#{pane_tty}')
+proc=$(ps -o comm= -t "$tty" | tail -n 1)
+
 dirs=`fd . ~ --type=d`
 
 selected=`echo "$dirs" | fzf --layout=reverse`
 
-tmux send-keys -t "$TMUX_PANE" 'cd ' "$selected" Enter ^L
+if [ -n "$selected" ]; then
+  if [ "$proc" = "zsh" ]; then
+    tmux send-keys -t "$TMUX_PANE" 'cd ' "$selected" Enter ^L
+  else
+    tmux neww -c "$selected"
+  fi
+fi
