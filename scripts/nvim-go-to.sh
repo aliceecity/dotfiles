45cd8301b6dd5a1bcfcf3f24807c53658e0d9976
alice <aliceecity@proton.me>
2026-01-13 14:07:35 +0100

    Removed ripgrep based filtering since it slows down by like 10x and maybe catches like 5 more files

--- scripts/nvim-go-to.sh
+++ scripts/nvim-go-to.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=`rg "" -l --no-ignore ~`
+files=`fd . ~ --type f -I | grep -Ev '\.(png|jpe?g|gif|webp|svg|pdf|zip|tar|gz|bz2|xz|7z|mp[34]|wav|flac|mkv|avi|exe|o|dll|so|bin|iso|dmg)$'`
 
 selected=`echo "$files" | fzf --layout=reverse`
 
