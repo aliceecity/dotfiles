b1f82feac8c5090339c48e1eb3a23663344ea88d
alice <aliceecity@proton.me>
2026-01-15 12:25:05 +0100

    Added functionality that highlights the search in nvim once the file is opened

--- scripts/rg-go-to.sh
+++ scripts/rg-go-to.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-selected=$(
-  fd . ~ --type=f | fzf --layout=reverse \
-                        --phony \
-                        --bind "change:reload:rg -l --no-ignore {q} ~ || true"
-  )
+result=$(
+  fd . ~ --type=f |
+    fzf --layout=reverse \
+        --phony \
+        --print-query \
+        --bind "change:reload:rg -l --no-ignore {q} ~ || true"
+)
+
+query=$(printf '%s\n' "$result" | sed -n '1p')
+selected=$(printf '%s\n' "$result" | sed -n '2p')
 
 if [ -n "$selected" ]; then
   filename=`echo "$selected" | awk -F "/" '{print $NF}'`
 
-  tmux neww -n "$filename" nvim "$selected"
+  tmux neww -n "$filename" nvim -c "/\\v$query" "$selected"
 fi
