8b110ec1cc9a882f239b0f89357677f99181c12f
alice <aliceecity@proton.me>
2026-01-28 09:59:12 +0100

    Filtering 'target' directory from rust projects

--- scripts/nvim-go-to.sh
+++ scripts/nvim-go-to.sh
@@ -6,7 +6,10 @@ if [ "$cwd" != "$HOME" ]; then
   filescurr=`fd . "$cwd" --type f -I -H`
 fi
 
-files=$(printf '%s\n' "$fileshome" "$filescurr" | grep -Ev '\.(png|jpe?g|gif|webp|svg|pdf|zip|tar|gz|bz2|xz|7z|mp[34]|wav|flac|mkv|avi|exe|o|class|dll|so|bin|iso|dmg)$')
+files=$(printf '%s\n' "$fileshome" "$filescurr" | \
+  grep -Ev '\.(png|jpe?g|gif|webp|svg|pdf|zip|tar|gz|bz2|xz|7z|mp[34]|wav|flac|mkv|avi|exe|o|class|dll|so|bin|iso|dmg)$' | \
+  grep -Ev '/target/'
+)
 
 selected=`echo "$files" | fzf --layout=reverse`
 
