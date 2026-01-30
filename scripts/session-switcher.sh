b705845eb4952d955b21b04a97a0238dcb12cbc0
alice <aliceecity@proton.me>
2026-01-30 12:35:03 +0100

    tmux session switcher script

---
+++ scripts/session-switcher.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sessions=$(tmux list-sessions | awk -F "(" '{print $1$3}' | tr -d ")")
+
+result=$(printf "%s\n" "$sessions" "new" | fzf --layout reverse | awk '{print $1}' | tr -d ":")
+
+if [ "$result" != "new" ]; then
+  tmux switch-client -t "$result"
+else
+  read -p "[name] " name
+  tmux new-session -d -s "$name"
+  tmux switch-client -t "$name"
+fi
