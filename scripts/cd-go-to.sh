6bdb2b46b5bb2826fe80069242c77b3f718b46b5
alice <aliceecity@proton.me>
2026-01-30 08:45:23 +0100

    Added hidden directory functionality thing

--- scripts/cd-go-to.sh
+++ scripts/cd-go-to.sh
@@ -3,7 +3,13 @@
 tty=$(tmux display-message -p '#{pane_tty}')
 proc=$(ps -o comm= -t "$tty" | tail -n 1)
 
-dirs=`fd . ~ --type=d -I`
+cwd=`tmux display-message -p '#{pane_current_path}'`
+dirshome=`fd . ~ --type d -I`
+if [ "$cwd" != "$HOME" ]; then
+  dirscurr=`fd . "$cwd" --type d -I -H`
+fi
+
+dirs=$(printf '%s\n' "$dirshome" "$dirscurr")
 
 selected=`echo "$dirs" | fzf --layout=reverse`
 
