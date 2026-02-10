cf7ec80b018698b937a1b8939008019eb25092c0
alice <aliceecity@proton.me>
2026-02-10 10:10:43 +0100

    Added esc functionality

--- scripts/session-switcher.sh
+++ scripts/session-switcher.sh
@@ -4,10 +4,12 @@ sessions=$(tmux list-sessions | awk -F "(" '{print $1$3}' | tr -d ")")
 
 result=$(printf "%s\n" "$sessions" "new" | fzf --layout reverse | awk '{print $1}' | tr -d ":")
 
-if [ "$result" != "new" ]; then
-  tmux switch-client -t "$result"
-else
-  read -p "[name] " name
-  tmux new-session -d -s "$name"
-  tmux switch-client -t "$name"
+if [ -n "$result" ]; then
+  if [ "$result" != "new" ]; then
+    tmux switch-client -t "$result"
+  else
+    read -p "[name] " name
+    tmux new-session -d -s "$name"
+    tmux switch-client -t "$name"
+  fi
 fi
