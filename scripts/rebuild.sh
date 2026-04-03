673c9938fcb94bcf00630aa02baf51976bf91452
alice <aliceecity@proton.me>
2026-04-03 16:26:09 +0200

    More robust last generation detection

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -8,7 +8,7 @@ tmux select-pane -t 1
 sudo nixos-rebuild switch --flake . &> nixos-switch.log || (
   cat nixos-switch.log | grep --color error && false)
 tmux kill-pane -t 2
-gen=$(nixos-rebuild list-generations | grep True | awk '{print $1,$2,$3}')
+gen=$(nixos-rebuild list-generations | sed -n '2p'| awk '{print $1,$2,$3}')
 git commit -am "$gen"
 
 popd >& /dev/null
