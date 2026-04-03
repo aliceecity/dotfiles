00b070aa051931d2fcbd94087971016fa71d84a3
alice <aliceecity@proton.me>
2026-04-03 16:30:24 +0200

    Use stdout instead of tmux panes for info

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -3,11 +3,8 @@
 pushd ~/dotfiles/nixos >& /dev/null
 
 git diff -U0 '*.nix'
-tmux split-window -v "watch -n .1 tail ~/dotfiles/nixos/nixos-switch.log"
-tmux select-pane -t 1
-sudo nixos-rebuild switch --flake . &> nixos-switch.log || (
+sudo nixos-rebuild switch --flake . | tee nixos-switch.log || (
   cat nixos-switch.log | grep --color error && false)
-tmux kill-pane -t 2
 gen=$(nixos-rebuild list-generations | sed -n '2p'| awk '{print $1,$2,$3}')
 git commit -am "$gen"
 
