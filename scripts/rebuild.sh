1bd042ea21866145bb421853dc74d93d0fc0967b
alice <aliceecity@proton.me>
2026-06-10 12:39:30 +0200

    Removed auto commit on rebuild

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -5,7 +5,5 @@ pushd ~/dotfiles/nixos >& /dev/null
 git diff -U0 '*.nix'
 sudo nixos-rebuild switch --flake . | tee nixos-switch.log || (
   cat nixos-switch.log | grep --color error && false)
-gen=$(nixos-rebuild list-generations | sed -n '2p'| awk '{print $1,$2,$3}')
-git commit -am "$gen"
 
 popd >& /dev/null
