7ea04ea4d3de9f9332fefbfdab0a700ef58d4f8c
alice <aliceecity@proton.me>
2026-08-23 01:02:04 +0200

    i forgot caps existed lol

--- scripts/nvim-go-to.sh
+++ scripts/nvim-go-to.sh
@@ -5,7 +5,7 @@ fileshome=`fd . ~ --type f -I`
 [[ "$cwd" != "$HOME" ]] && filescurr=`fd . "$cwd" --type f -I -H`
 
 files=$(printf '%s\n' "$fileshome" "$filescurr" | \
-  grep -Ev '\.([Gg]bx|png|jpe?g|gif|webp|svg|pdf|zip|tar|gz|bz2|xz|7z|mp[34]|m4a|wav|flac|mkv|avi|jar|exe|o|class|dll|so|bin|iso|dmg)$' | \
+  grep -Ev --ignore-case '\.(gbx|png|jpe?g|gif|webp|svg|pdf|zip|tar|gz|bz2|xz|7z|mp[34]|m4a|wav|flac|mkv|avi|jar|exe|o|class|dll|so|bin|iso|dmg)$' | \
   grep -Ev '/(instances|jason|target|records)/' | \
   sort -u | \
   sed '/^$/d'
--- scripts/open.sh
+++ scripts/open.sh
@@ -5,7 +5,7 @@ fileshome=`fd . ~ --type f -I`
 [[ "$cwd" != "$HOME" ]] && filescurr=`fd . "$cwd" --type f -I -H`
 
 files=$(printf '%s\n' "$fileshome" "$filescurr" | \
-  grep -Ev '\.(bin|class|dat|dds|diff|gpg|gradle|gz|jar|java|js|l|y|mas2j|old|output|[Gg]bx|sh|c|rs|nix|md|lock|o|toml|h|lua|asl|py|json|rasi|html|css|conf|jsonc|log)$' | \
+  grep -Ev --ignore-case '\.(bin|class|dat|dds|diff|gpg|gradle|gz|jar|java|js|l|y|mas2j|old|output|[Gg]bx|sh|c|rs|nix|md|lock|o|toml|h|lua|asl|py|json|rasi|html|css|conf|jsonc|log)$' | \
   grep -Ev '^[^\.]*$' | \
   grep -Ev '/(instances|jason|target|records)/' | \
   sort -u | \
