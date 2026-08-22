f2fef97081fdcb6cef896780867adb87781e7b67
alice <aliceecity@proton.me>
2026-08-22 21:41:46 +0200

    i like opening files and shi

---
+++ scripts/open.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cwd=`tmux display-message -p '#{pane_current_path}'`
+fileshome=`fd . ~ --type f -I`
+[[ "$cwd" != "$HOME" ]] && filescurr=`fd . "$cwd" --type f -I -H`
+
+files=$(printf '%s\n' "$fileshome" "$filescurr" | \
+  grep -Ev '\.(bin|class|dat|dds|diff|gpg|gradle|gz|jar|java|js|l|y|mas2j|old|output|[Gg]bx|sh|c|rs|nix|md|lock|o|toml|h|lua|asl|py|json|rasi|html|css|conf|jsonc|log)$' | \
+  grep -Ev '^[^\.]*$' | \
+  grep -Ev '/(instances|jason|target|records)/' | \
+  sort -u | \
+  sed '/^$/d'
+)
+
+selected=`echo "$files" | fzf --layout=reverse`
+
+if [[ -n "$selected" ]]; then
+    tmux run-shell "nohup xdg-open $(printf '%q' "$selected") >/dev/null 2>&1 &"
+fi
