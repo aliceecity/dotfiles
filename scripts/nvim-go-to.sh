49ef7911140d489b014a20af54818befeb66198a
alice <aliceecity@proton.me>
2026-09-04 00:31:45 +0200

    directories in nvim-go-to

--- scripts/nvim-go-to.sh
+++ scripts/nvim-go-to.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cwd=`tmux display-message -p '#{pane_current_path}'`
-fileshome=`fd . ~ --type f -I`
-[[ "$cwd" != "$HOME" ]] && filescurr=`fd . "$cwd" --type f -I -H`
+fileshome=`fd . ~ -I`
+[[ "$cwd" != "$HOME" ]] && filescurr=`fd . "$cwd" -I -H`
 
 files=$(printf '%s\n' "$fileshome" "$filescurr" | \
   grep -Ev --ignore-case '\.(gbx|png|jpe?g|gif|webp|svg|pdf|zip|tar|gz|bz2|xz|7z|mp[34]|m4a|wav|flac|mkv|avi|jar|exe|o|class|dll|so|bin|iso|dmg)$' | \
@@ -11,6 +11,6 @@ files=$(printf '%s\n' "$fileshome" "$filescurr" | \
   sed '/^$/d'
 )
 
-selected=`echo "$files" | fzf --layout=reverse --preview='bat --color=always --style=plain {}'`
+selected=`echo "$files" | fzf --layout=reverse --preview='[[ -d {} ]] && ls --color=always {} || bat --color=always --style=plain {}'`
 
-[[ -n "$selected" ]] && tmux neww -n "${selected##*/}" nvim "$selected"
+[[ -n "$selected" ]] && tmux neww -n $(basename "$selected") nvim "$selected"
